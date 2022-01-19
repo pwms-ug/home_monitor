7693802b93e4e30bd224f186a05f64c10b5febcf
simonojok19 <simonojok19@gmail.com>
2022-01-19 21:20:05 +0300

    finis meter reading api

--- node-rest-api/models/MeterReadingModel.js
+++ node-rest-api/models/MeterReadingModel.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 module.exports = mongoose.model('MeterReading', new mongoose.Schema({
     meterNumber: {type: String},
     date: {type: String},
-    reading: {type: String},
+    reading: {type: Number},
     location: {type: String}
 }));
