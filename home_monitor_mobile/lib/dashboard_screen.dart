03357460d324a45d62d54ccfdeda1023bb3cd48c
simonpojok <simonpojok@gmail.com>
2022-02-14 12:37:59 +0300

    fixed some graph issue

--- home_monitor_mobile/lib/dashboard_screen.dart
+++ home_monitor_mobile/lib/dashboard_screen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:charts_flutter/flutter.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter_notion_budget/configure_meter_screen.dart';
 
@@ -20,85 +21,147 @@ class _DashboardScreenState extends State<DashboardScreen> {
         ),
         body: Container(
           padding: EdgeInsets.all(10),
-          child: GridView.count(
-            crossAxisCount: 3,
+          child: Column(
             children: [
-              InkWell(
-                onTap: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (builder) => AddMeterScreen(),
-                    ),
-                  )
-                },
-                child: Card(
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.add,
-                          size: 34,
-                        ),
-                        Text(
-                          "Configure\nMeter",
-                          textAlign: TextAlign.center,
+              Container(
+                height: 120,
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: [
+                    InkWell(
+                      onTap: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (builder) => AddMeterScreen(),
+                          ),
                         )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () => {},
-                child: Card(
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.add,
-                          size: 34,
+                      },
+                      child: Card(
+                        child: Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: 34,
+                              ),
+                              Text(
+                                "Configure\nMeter",
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
                         ),
-                        Text("Add Meter")
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (builder) => MeterReadingScreen(),
-                    ),
-                  )
-                },
-                child: Card(
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.read_more,
-                          size: 34,
+                    InkWell(
+                      onTap: () => {},
+                      child: Card(
+                        child: Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: 34,
+                              ),
+                              Text("Add Meter")
+                            ],
+                          ),
                         ),
-                        Text(
-                          "Meter\nReadings",
-                          textAlign: TextAlign.center,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (builder) => MeterReadingScreen(),
+                          ),
                         )
-                      ],
+                      },
+                      child: Card(
+                        child: Container(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.read_more,
+                                size: 34,
+                              ),
+                              Text(
+                                "Meter\nReadings",
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                height: 200,
+                child: SimpleTimeSeriesChart.withSampleData(),
+              )
             ],
           ),
         ));
   }
+}
+
+class SimpleTimeSeriesChart extends StatelessWidget {
+  final List<Series<dynamic, DateTime>> seriesList;
+
+  SimpleTimeSeriesChart(this.seriesList);
+
+  /// Creates a [TimeSeriesChart] with sample data and no transition.
+  factory SimpleTimeSeriesChart.withSampleData() {
+    return new SimpleTimeSeriesChart(
+      _createSampleData(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.TimeSeriesChart(
+      seriesList,
+      animate: true,
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
+    );
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    final data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 19), 5),
+      new TimeSeriesSales(new DateTime(2017, 9, 26), 25),
+      new TimeSeriesSales(new DateTime(2017, 10, 3), 100),
+      new TimeSeriesSales(new DateTime(2017, 10, 10), 75),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.time,
+        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime time;
+  final int sales;
+
+  TimeSeriesSales(this.time, this.sales);
 }
