572e69a9d6423edd3f818ee92dd8bb176e045c05
simonpojok <simonpojok@gmail.com>
2022-03-30 21:45:25 +0300

    fixed

--- app.js
+++ app.js
@@ -14,10 +14,26 @@ const port = process.env.PORT || 3000;
 mongoose.connect('mongodb+srv://express_backend:express_backend@cluster0.wdx4a.mongodb.net/express_backend_v5?retryWrites=true&w=majority');
 const db = mongoose.connection;
 
+const OrderModel = mongoose.model('ProductOrder', new mongoose.Schema({
+    primaryKey: { type: Number },
+    id: { type: String },
+    customerName: { type: String },
+    totalPrice: { type: Number },
+    dateTime: { type: String },
+    syncStatus: { type: Boolean, default: false },
+}));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
+app.use('/orders', (req, res) => {
+    console.log("Running here")
+    const order = new OrderModel(req.body)
+    delete order.syncStatus
+
+    res.send(order)
+})
 app.use('/api/orders', OrderRouter);
 app.use('/api/meter_readings', MeterReadingRouter);
 app.get('/', (req, res) => {
